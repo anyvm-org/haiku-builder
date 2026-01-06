72a63055d3227ee92fa2284b3027b319656ab13a
neil <gitpc@neilpang.com>
2026-01-06 20:44:55 +0100

    fix

--- hooks/enablessh.sh
+++ hooks/enablessh.sh
@@ -1,9 +1,19 @@
 
 
+bash ./vbox.sh  pauseVNC
+
+vncdo key right
+vncdo key right
+
 sleep 60;
+
 vncdo key right
 vncdo key right
-vncdo key lsuper-alt-t
+
+vncdotool key super-alt-t
+sleep 2;
+
+vncdotool key super-alt-t
 
 sleep 30;
 
